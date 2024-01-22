ffe1b2a09b6359034608de927f9b2f9285b137af
Saadmrp1038 <saadmrp222@gmail.com>
2024-01-22 13:52:59 +0600

    type fix

--- src/routes/home/+layout.server.ts
+++ src/routes/home/+layout.server.ts
@@ -1,17 +1,19 @@
-export const load = async ({ locals: {supabase } ,fetch}) => {
-	const { data: { user } } = await supabase.auth.getUser()
+export const load = async ({ locals: { supabase }, fetch }) => {
+	const {
+		data: { user }
+	} = await supabase.auth.getUser();
 	// console.log(user)
 
-	const email = user?.email
+	const email = user?.email;
 
 	const res = await fetch('/api/user', {
 		method: 'POST',
-		body: JSON.stringify({ email: email})
-	}) 
-	const data = await res.json()deploy
+		body: JSON.stringify({ email: email })
+	});
+	const data = await res.json();
 	// console.log(data)
 
 	return {
 		user: data
-	}
+	};
 };
